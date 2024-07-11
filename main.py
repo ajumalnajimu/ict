3390988977f7ccf2c400378fcb68b15d93f4421a
ajumalnajimu <ajumxl@gmail.com>
2024-07-11 13:01:53 +0530

    HTML

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from bank_operations.bank import credit,debit,transfer
 print("Hello World ")
 print("New program")
+
+
+def say_color(self):
+    print("My color is",self.color)
+
+class Apple(Fruits):
+    def my_type(self):
+        print("I am apple..")
+a = Apple("Apple1","red")
+print(a.color)                
